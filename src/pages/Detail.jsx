643d7d4f0f0d8aa9240a5fae5490c34f019339e5
“Yana <yana@particularaudience.com>
2022-06-20 08:56:35 +1000

    Added alert on detail page

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,10 +1,26 @@
-import { Navbar, Container, Nav, Button } from 'react-bootstrap'
-import { useParams } from "react-router-dom"
+import { Button, Alert } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+
 export default function Detail({ veggies }) {
+
+    useEffect(() => {
+        setTimeout(() => {
+            setVisible(false)
+        }, 3000)
+    }, []
+    )
+
+
     let { id } = useParams()
+    let [visible, setVisible] = useState(true)
+
     const veggie = veggies.find((veggie) => veggie.id == id)
     return (
         <div>
+            {visible ? <Alert key={'warning'} variant={'warning'}>
+                Free delivery for an hour!
+        </Alert> : null}
             <img className="veggie-detail-img" src={veggie.img} alt="veggie-product-img" />
             <h4>{veggie.title}</h4>
             <h4>{veggie.price}</h4>
